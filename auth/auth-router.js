6cd8998afb411650ce296c61e5ec0570d2a36b93
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-01-19 19:56:23 -0600

    add sessions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,50 +1,67 @@
-const bcrypt = require("bcryptjs")
-const express = require("express")
-const usersModel = require("../users/users-model")
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const restricted = require('../middleware/restricted');
+const usersModel = require('../users/users-model');
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
+router.post('/register', async (req, res, next) => {
   try {
-    const saved = await usersModel.add(req.body)
-    
-    res.status(201).json(saved)
+    const saved = await usersModel.add(req.body);
+
+    res.status(201).json(saved);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.post("/login", async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   try {
-    const { username, password } = req.body
-    const user = await usersModel.findBy({ username }).first()
+    const { username, password } = req.body;
+    const user = await usersModel.findBy({ username }).first();
     // since bcrypt hashes generate different results due to the salting,
     // we rely on the magic internals to compare hashes (rather than doing
     // it manulally by re-hashing and comparing)
-    const passwordValid = await bcrypt.compare(password, user.password)
+    const passwordValid = await bcrypt.compare(password, user.password);
 
     if (user && passwordValid) {
+      // stores the user data in the current session,
+      // so it persists between requests
+      req.session.user = user;
+
       res.status(200).json({
         message: `Welcome ${user.username}!`,
-      })
+      });
     } else {
       res.status(401).json({
-        message: "Invalid Credentials",
-      })
+        message: 'Invalid Credentials',
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get("/protected", async (req, res, next) => {
+router.get('/protected', restricted(), async (req, res, next) => {
   try {
     res.json({
-      message: "You are authorized",
-    })
+      message: 'You are authorized',
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = router
+router.get('/logout', restricted(), (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.json({
+        message: 'You are logged out',
+      });
+    }
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-const express = require("express")
-const helmet = require("helmet")
-const cors = require("cors")
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
 
-const authRouter = require("./auth/auth-router")
-const usersRouter = require("./users/users-router")
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
 
-const server = express()
-const port = process.env.PORT || 5000
+const server = express();
+const port = process.env.PORT || 5000;
 
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(
+  session({
+    resave: false, // keep it false to avoid sessions that have not changed
+    saveUninitialized: false, // GDPR laws against setting cookies automatically
+    secret: 'keep it secret, keep it safe!', // to cryptographically sign the cookie
+    cookie: {
+      httpOnly: true, // cannot access the cookie from JS (more secure)
+      maxAge: 1000 * 60 * 60 * 24 * 7, // expire the session after 7 days
+      secure: false, // in production, this should be true so the cookie header is encrypted
+    },
+  }),
+);
 
-server.use("/auth", authRouter)
-server.use("/users", usersRouter)
+server.use('/auth', authRouter);
+server.use('/users', usersRouter);
 
-server.get("/", (req, res, next) => {
+server.get('/', (req, res, next) => {
+  console.log(req.headers);
   res.json({
-    message: "Welcome to our API",
-  })
-})
+    message: 'Welcome to our API',
+  });
+});
 
 server.use((err, req, res, next) => {
-  console.log("Error:", err)
+  console.log('Error:', err);
 
   res.status(500).json({
-    message: "Something went wrong",
-  })
-})
-
+    message: 'Something went wrong',
+  });
+});
 
 server.listen(port, () => {
-  console.log(`\n** Running on http://localhost:${port} **\n`)
-})
+  console.log(`\n** Running on http://localhost:${port} **\n`);
+});
